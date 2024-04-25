ad03f1a31ab2a1fc2cc2eb1391fd586ab406167a
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-25 13:51:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,3 +5,27 @@ function longestSubstring(s) {
 module.exports = { longestSubstring };
 
 
+function longestSubstring(s) {
+    let maxLength = 0;
+    let start = 0;
+    let charIndexMap = {};
+
+    for (let end = 0; end < s.length; end++) {
+        const char = s[end];
+        
+        // If the character is already in the current substring, update the start pointer
+        if (charIndexMap[char] !== undefined && charIndexMap[char] >= start) {
+            start = charIndexMap[char] + 1;
+        }
+
+        // Update the index of the current character
+        charIndexMap[char] = end;
+
+        // Update the maximum length if needed
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
+}
+
+module.exports = { longestSubstring };
