df6be66c930deb401eed6f40540f83c7d1298bbe
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-25 13:40:29 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,14 +1,8 @@
-function smallestMissingPositiveInteger(nums) {
-
-  // Implement the function smallest_missing_positive_integer
-
-}
-
 module.exports = smallestMissingPositiveInteger;
 function smallestMissingPositiveInteger(nums) {
   // Filter out non-positive integers and duplicates
-  const uniquePositives = [...new Set(nums.filter(num => num > 0))];
-  
+  const uniquePositives = [...new Set(nums.filter((num) => num > 0))];
+
   // Sort the array
   uniquePositives.sort((a, b) => a - b);
 
@@ -17,13 +11,13 @@ function smallestMissingPositiveInteger(nums) {
 
   // Loop through sorted positive integers
   for (let i = 0; i < uniquePositives.length; i++) {
-      // If the current number is equal to the smallest missing positive, increment smallestMissing
-      if (uniquePositives[i] === smallestMissing) {
-          smallestMissing++;
-      } else {
-          // If the current number is greater than the smallest missing positive, return smallestMissing
-          return smallestMissing;
-      }
+    // If the current number is equal to the smallest missing positive, increment smallestMissing
+    if (uniquePositives[i] === smallestMissing) {
+      smallestMissing++;
+    } else {
+      // If the current number is greater than the smallest missing positive, return smallestMissing
+      return smallestMissing;
+    }
   }
 
   // If all numbers are in sequence without any gap, return the next integer after the last one
