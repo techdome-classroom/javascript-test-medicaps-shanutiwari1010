86766b6d2fec2de633c3a7bb5762693f9e089110
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-25 13:51:07 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,22 +5,3 @@
 // module.exports = { longestSubstring };
 
 
-function lengthOfLongestSubstring(s) {
-    let maxLength = 0;
-    let left = 0;
-    const charIndexMap = {};
-
-    for (let right = 0; right < s.length; right++) {
-        const currentChar = s[right];
-        if (charIndexMap[currentChar] !== undefined && charIndexMap[currentChar] >= left) {
-            
-            left = charIndexMap[currentChar] + 1;
-        }
-       
-        charIndexMap[currentChar] = right;
-        
-        maxLength = Math.max(maxLength, right - left + 1);
-    }
-
-    return maxLength;
-}
